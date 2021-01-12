a05afc8e00136584e0ec5e3778c53f7360dddc8c
qq1176321897 <1176321897@qq.com>
2021-01-12 14:47:07 +0800

    fix typo

--- __init__.py
+++ __init__.py
@@ -183,7 +183,7 @@ async def on_arena_schedule():
             if res[1] < last[1] and info['grand_arena_on']:
                 await bot.send_group_msg(
                     group_id = int(binds[user]['gid']),
-                    message = f'[CQ:at,qq={user}]您的竞技场排名发生变化：{last[0]}->{res[0]}'
+                    message = f'[CQ:at,qq={user}]您的公主竞技场排名发生变化：{last[1]}->{res[1]}'
                 )
         except Exception as e:
             sv.logger.info(f'对{binds[user]["id"]}的检查出错\n{e}')
