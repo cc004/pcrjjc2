29d82dfedbb957fd16060dbc8ceb84788723e763
穿越电线 <71607036+CYDXDianXian@users.noreply.github.com>
2022-02-25 11:08:43 +0800

    解决AttributeError报错，#65

--- __init__.py
+++ __init__.py
@@ -77,7 +77,7 @@ async def query(id: str):
                 'target_viewer_id': int(id)
             }))
         return res
-    
+
 def save_binds():
     with open(config, 'w') as fp:
         dump(root, fp, indent=4)
@@ -148,7 +148,8 @@ async def on_query_arena(bot, ev):
 f'''昵称：{res['user_info']["user_name"]}
 jjc排名：{res['user_info']["arena_rank"]}
 pjjc排名：{res['user_info']["grand_arena_rank"]}
-最后登录：{last_login_str}''', at_sender=False)
+最后登录：{last_login_str}
+''', at_sender=False)
         except ApiException as e:
             await bot.finish(ev, f'查询出错，{e}', at_sender=True)
 
@@ -249,7 +250,7 @@ async def send_arena_sub_status(bot,ev):
     公主竞技场订阅：{'开启' if info['grand_arena_on'] else '关闭'}''',at_sender=True)
 
 
-@sv.scheduled_job('interval', minutes=1) # minutes是刷新频率，可按自身服务器性能输入其他数值，可支持整数、小数
+@sv.scheduled_job('interval', minutes=3) # minutes是刷新频率，可按自身服务器性能输入其他数值，可支持整数、小数
 async def on_arena_schedule():
     global cache, binds, lck
     bot = get_bot()
