c4acc5d46b2a262b0cedd3798aa70131e334005d
穿越电线 <lx839071800@163.com>
2022-08-23 17:45:20 +0800

    接入hoshinobot敏感词库，以防bot被恶意利用

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from json import load, dump
 import json
 from nonebot import get_bot, on_command
-from hoshino import priv
+from hoshino import priv, util
 from hoshino.typing import NoticeSession, MessageSegment
 from .pcrclient import pcrclient, ApiException, bsdkclient
 from asyncio import Lock
@@ -210,7 +210,7 @@ async def on_query_arena(bot, ev):
             last_login_str = time.strftime('%Y-%m-%d %H:%M:%S',last_login_date)
             
             await bot.finish(ev, 
-f'''昵称：{res['user_info']["user_name"]}
+f'''昵称：{util.filt_message(str(res['user_info']["user_name"]))}
 jjc排名：{res['user_info']["arena_rank"]}
 pjjc排名：{res['user_info']["grand_arena_rank"]}
 最后登录：{last_login_str}''', at_sender=False)
